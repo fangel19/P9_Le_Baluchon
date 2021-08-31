878788cc0882e075af2f5245e698e9ea572aa50b
fangel19 <fournyangel@msn.com>
2021-08-31 20:25:34 +0200

    New test MockNetworkCallsTests in CashServicesTests add static in KakeResponseData

--- P9_01_Le_BaluchonTests/CashTests/CashServiceTests.swift
+++ P9_01_Le_BaluchonTests/CashTests/CashServiceTests.swift
@@ -10,86 +10,165 @@ import XCTest
 
 class MockNetworkCallsTests: XCTestCase {
     
-    func testGetCashShouldPostFailedCallbackIfError() {
-        // Given
-        let cashService = CashService(cashSession: URLSessionFake(data: nil, response: nil, error: FakeResponseData.error))
-        // When
-        let expectation = XCTestExpectation(description: "Loading URL")
+    var cashService: CashService!
+    
+    override func setUp() {
+        super.setUp()
         
-        cashService.getCash { (success, cash) in
-            // Then
-            XCTAssertFalse(success)
-            XCTAssertNil(cash)
-            expectation.fulfill()
+        URLTestProtocol.loadingHandler = { request in
+            
+            let response: HTTPURLResponse = FakeResponseData.responseOK
+            let error: Error? = nil
+            let data = FakeResponseData.cashCorrectData
+            return (response, data, error)
         }
         
-        wait(for: [expectation], timeout: 0.01)
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLTestProtocol.self]
+        let session = URLSession(configuration: configuration)
+        
+        cashService  = CashService(cashSession: session)
+    }
+    
+    func testGetCashShouldPostFailedCallbackIfError() {
+        
+        URLTestProtocol.loadingHandler = { request in
+            
+            let response: HTTPURLResponse = FakeResponseData.responseKO
+            let error: Error? = FakeResponseData.conversionError
+            let data: Data? = nil
+            return (response, data, error)
+        }
+        
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLTestProtocol.self]
+        let session = URLSession(configuration: configuration)
+        
+        cashService  = CashService(cashSession: session)
+        
+        let expectation  = XCTestExpectation(description: "Wait for queue change.")
+        
+        cashService.getCash { (result) in
+            
+            guard case .failure(let error) = result else {
+                XCTFail("fail")
+                return
+            }
+            XCTAssertNotNil(error)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
     }
     
     func testGetCashShouldPostFailedCallbackNoData() {
-        // Given
-        let cashService = CashService(cashSession: URLSessionFake(data: nil, response: nil, error: nil))
-        // When
-        let expectation = XCTestExpectation(description: "Loading URL")
         
-        cashService.getCash { (success, cash) in
-            // Then
-            XCTAssertFalse(success)
-            XCTAssertNil(cash)
-            expectation.fulfill()
-        }
         
-        wait(for: [expectation], timeout: 0.01)
-    }
-    
-    func testGetCashShouldPostFailedCallbackIfIncorrectResponse() {
-        // Given
-        let cashService = CashService(cashSession: URLSessionFake(data: FakeResponseData.cashCorrectData, response: FakeResponseData.responseKO, error: nil))
-        // When
-        let expectation = XCTestExpectation(description: "Loading URL")
-        
-        cashService.getCash { (success, cash) in
-            // Then
-            XCTAssertFalse(success)
-            XCTAssertNil(cash)
-            expectation.fulfill()
-        }
-        
-        wait(for: [expectation], timeout: 0.01)
+        //        // Given
+        //        let cashService = CashService(cashSession: URLSessionFake(data: nil, response: nil, error: nil))
+        //        // When
+        //        let expectation = XCTestExpectation(description: "Wait for queue change.")
+        //
+        //        cashService.getCash { (success, cash) in
+        //            // Then
+        //            XCTAssertFalse(success)
+        //            XCTAssertNil(cash)
+        //            expectation.fulfill()
+        //        }
+        //
+        //        wait(for: [expectation], timeout: 0.01)
     }
     
     func testGetCashShouldPostFailedCallbackIfIncorrectData() {
-        // Given
-        let cashService = CashService(cashSession: URLSessionFake(data: FakeResponseData.IncorrectData, response: FakeResponseData.responseOK, error: nil))
-        // When
-        let expectation = XCTestExpectation(description: "Loading URL")
         
-        cashService.getCash { (success, cash) in
-            // Then
-            XCTAssertFalse(success)
-            XCTAssertNil(cash)
+        URLTestProtocol.loadingHandler = { request in
+            
+            let response: HTTPURLResponse = FakeResponseData.responseOK
+            let error: Error? = nil
+            let data: Data? = FakeResponseData.IncorrectData
+            return (response, data, error)
+        }
+        
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLTestProtocol.self]
+        let session = URLSession(configuration: configuration)
+        
+        cashService  = CashService(cashSession: session)
+        
+        let expectation  = XCTestExpectation(description: "Wait for queue change.")
+        
+        cashService.getCash { (result) in
+            
+            guard case .failure(let error) = result else {
+                XCTFail("fail")
+                return
+            }
+            
+            XCTAssertNotNil(error)
             expectation.fulfill()
         }
         
-        wait(for: [expectation], timeout: 0.01)
+        wait(for: [expectation], timeout: 1)
+        
+    }
+    
+    func testGetCashShouldPostFailedCallbackIfIncorrectResponse() {
+        
+        URLTestProtocol.loadingHandler = { request in
+            
+            let response: HTTPURLResponse = FakeResponseData.responseKO
+            let error: Error? = nil
+            let data: Data? = FakeResponseData.cashCorrectData
+            return (response, data, error)
+        }
+        
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLTestProtocol.self]
+        let session = URLSession(configuration: configuration)
+        
+        cashService  = CashService(cashSession: session)
+        
+        let expectation  = XCTestExpectation(description: "Wait for queue change.")
+        
+        cashService.getCash { (result) in
+            
+            guard case .failure(let error) = result else {
+                XCTFail("fail")
+                return
+            }
+            XCTAssertNotNil(error)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
     }
     
     func testGetCashShouldPostSuccessCallbackIfNoErrorAndCorrectData() {
-        // Given
-        let cashService = CashService(cashSession: URLSessionFake(data: FakeResponseData.cashCorrectData, response: FakeResponseData.responseOK, error: nil))
-        // When
-        let expectation = XCTestExpectation(description: "Loading URL")
         
-        cashService.getCash { (success, cash) in
-            // Then
-            let rates = ["USD", 1.17]
-            XCTAssertTrue(success)
-            XCTAssertNotNil(cash)
-            XCTAssertEqual(rates, cash!.rates)
-            expectation.fulfill()
+        URLTestProtocol.loadingHandler = { request in
+            
+            let response: HTTPURLResponse = FakeResponseData.responseOK
+            let error: Error? = nil
+            let data: Data? = FakeResponseData.cashCorrectData
+            return (response, data, error)
         }
         
-        wait(for: [expectation], timeout: 0.01)
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLTestProtocol.self]
+        let session = URLSession(configuration: configuration)
+        
+        cashService  = CashService(cashSession: session)
+        
+        let expectation  = XCTestExpectation(description: "Wait for queue change.")
+        
+        cashService.getCash { (result) in
+            
+            guard case .success(let cashInfo) = result else {
+                XCTFail("fail")
+                return
+            }
+            XCTAssertEqual(cashInfo.rates["USD"], 1.17)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
     }
-    
 }
+
--- P9_01_Le_BaluchonTests/FakeResponseData.swift
+++ P9_01_Le_BaluchonTests/FakeResponseData.swift
@@ -11,24 +11,25 @@ class FakeResponseData {
     
     // MARK: - Response
     
-    let responseOK = HTTPURLResponse(url: URL(string: "https://openclassrooms.com")!, statusCode: 200, httpVersion: nil, headerFields: nil)!
+    static let responseOK = HTTPURLResponse(url: URL(string: "https://openclassrooms.com")!, statusCode: 200, httpVersion: nil, headerFields: nil)!
     
-    let responseKO = HTTPURLResponse(url: URL(string: "https://openclassrooms.com")!, statusCode: 500, httpVersion: nil, headerFields: nil)!
+    static let responseKO = HTTPURLResponse(url: URL(string: "https://openclassrooms.com")!, statusCode: 500, httpVersion: nil, headerFields: nil)!
     
     // MARK: - Data
     
-    var cashCorrectData: Data {
+    static var cashCorrectData: Data {
         let bundle = Bundle(for: FakeResponseData.self)
         let url = bundle.url(forResource: "Cash", withExtension: "json")
         let data = try! Data(contentsOf: url!)
         return data
     }
     
-    let IncorrectData = "erreur" .data(using: .utf8)!
+    static let IncorrectData = "erreur" .data(using: .utf8)!
     
     // MARK: - Error
      
-     class TestError: Error {}
+    
+     class ConversionError: Error {}
      
-     let error = TestError()
+     static let conversionError = ConversionError()
 }
